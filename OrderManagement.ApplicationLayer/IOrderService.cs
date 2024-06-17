315d5fce60d2b178c1d3b21cf5cead7ec265cea6
srivallabh joshi <srivallabhjoshi13@gmail.com>
2024-06-17 16:00:22 +0530

    Started Application Layer

---
+++ OrderManagement.ApplicationLayer/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.ApplicationLayer
+{
+    internal interface IOrderService
+    {
+        // create order
+        //create and add to the DB. -> call AddAsync. -> -> product,stock ->  valid -> avail -> call inventory update -> email service (trigger) 
+
+    }
+}
---
+++ OrderManagement.ApplicationLayer/OrderService.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using OrderManagement.DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.ApplicationLayer
+{
+    internal class OrderService : IOrderService
+    {
+        public OrderService() { 
+    }
+}
